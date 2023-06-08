bf65e2099f251fc6f183285ae8547c4a8b62983f
tadotcom <kh.sk.ti.1820@gmail.com>
2023-06-08 23:37:35 +0900

    MainActivityからFragmentを表示するように記載

--- app/src/main/java/com/example/reminder/MainActivity.kt
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = MainFragment()
+        fragmentTransaction.add(R.id.fragment, fragment)
+        fragmentTransaction.commit()
     }
 }

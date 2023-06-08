588fc67b794f76585b125b9a6570b5f7cb16151c
tadotcom <kh.sk.ti.1820@gmail.com>
2023-06-08 23:30:41 +0900

    MainFragmentを追加

---
+++ app/src/main/java/com/example/reminder/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.example.reminder
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}

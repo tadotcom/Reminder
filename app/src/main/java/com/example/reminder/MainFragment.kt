332203642ce474c4b30c750054730509210c5ece
tadotcom <kh.sk.ti.1820@gmail.com>
2023-06-11 03:34:28 +0900

    MainFragmentにバイディングの変数とViewModelを紐付ける変数を追加

--- app/src/main/java/com/example/reminder/MainFragment.kt
+++ app/src/main/java/com/example/reminder/MainFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.reminder.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

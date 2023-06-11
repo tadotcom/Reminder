eda845ec4def691e1da0e91c1ac384e70affdc1e
tadotcom <kh.sk.ti.1820@gmail.com>
2023-06-12 00:14:24 +0900

    FireBaseからデータを取得するfetchTfetchTaskを追加

--- app/src/main/java/com/example/reminder/TaskRepository.kt
+++ app/src/main/java/com/example/reminder/TaskRepository.kt
@@ -1,4 +1,29 @@
 package com.example.reminder
 
+import android.service.controls.ControlsProviderService.TAG
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+
+
 class TaskRepository {
+
+    private var database = FirebaseFirestore.getInstance()
+
+    //データを取得する
+    fun fetchTask() {
+        val docRef = database.collection("cities").document("SF")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
+    }
 }

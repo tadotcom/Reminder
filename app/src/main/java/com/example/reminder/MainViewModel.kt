e6b12841afc2d1fca70d0e64d1949330c56bca6c
tadotcom <kh.sk.ti.1820@gmail.com>
2023-06-11 22:42:28 +0900

    MainViewModelにタスクのデータを管理する変数を追加

--- app/src/main/java/com/example/reminder/MainViewModel.kt
+++ app/src/main/java/com/example/reminder/MainViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.reminder
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.sql.Time
+import java.util.Calendar
 
 class MainViewModel : ViewModel() {
+
+    private val repository = TaskRepository()
+    val taskTitle: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    val taskDetail: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    val taskCalender: MutableLiveData<Calendar> by lazy { MutableLiveData<Calendar>() }
+    val taskTime: MutableLiveData<Time> by lazy { MutableLiveData<Time>() }
+    val taskImportance: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+
+    fun fetchTask() {}
 }

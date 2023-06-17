092ce4a78e5f6d13b51e48551674668b8827794c
tadotcom <kh.sk.ti.1820@gmail.com>
2023-06-17 16:26:10 +0900

    TaskDetailViewModelにボタンクリック検知用の変数とメソッドを追加

--- app/src/main/java/com/example/reminder/TaskDetailViewModel.kt
+++ app/src/main/java/com/example/reminder/TaskDetailViewModel.kt
@@ -1,6 +1,20 @@
 package com.example.reminder
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TaskDetailViewModel : ViewModel()  {
+
+    val deleteBtnLiveData: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
+    val editBtnLiveData: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
+
+    //削除ボタン
+    fun deleteBntClick() {
+        deleteBtnLiveData.value = true
+    }
+
+    //編集ボタン
+    fun editBtnClick() {
+        editBtnLiveData.value = true
+    }
 }

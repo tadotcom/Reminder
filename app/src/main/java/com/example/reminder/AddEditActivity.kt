560d7b8fac68644d38b4d135e9c6d938d111e4a2
tadotcom <kh.sk.ti.1820@gmail.com>
2023-06-17 21:14:58 +0900

    AddEditFragmentを表示する

--- app/src/main/java/com/example/reminder/AddEditActivity.kt
+++ app/src/main/java/com/example/reminder/AddEditActivity.kt
@@ -7,5 +7,11 @@ class AddEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = AddEditFragment()
+        fragmentTransaction.add(R.id.addedit_fragment, fragment)
+        fragmentTransaction.commit()
     }
 }

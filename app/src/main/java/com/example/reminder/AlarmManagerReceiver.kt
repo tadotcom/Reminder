bdb4671b22c491536f4d1b76c7dd475c2a541e18
tadotcom <kh.sk.ti.1820@gmail.com>
2023-06-18 13:39:06 +0900

    アラームを通知するAlarmManagerReceiverを追加、マニフェストファイルにも追加

---
+++ app/src/main/java/com/example/reminder/AlarmManagerReceiver.kt
@@ -0,0 +1,18 @@
+package com.example.reminder
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+
+class AlarmManagerReceiver: BroadcastReceiver() {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // Alarmの区別と送信するデータの番号
+        val id = intent?.getIntExtra("id", 0)
+        Toast.makeText(context, "アラーム番号　：「{$id}」が実行されました", Toast.LENGTH_LONG).show()
+    }
+}

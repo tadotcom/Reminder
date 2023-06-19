6379c18843793f567b727bf2fbf1853ef0d25479
tadotcom <kh.sk.ti.1820@gmail.com>
2023-06-19 21:59:46 +0900

    RecyclerAdapterのダミーデータと表示するテキストを修正

--- app/src/main/java/com/example/reminder/RecyclerAdapter.kt
+++ app/src/main/java/com/example/reminder/RecyclerAdapter.kt
@@ -18,7 +18,8 @@ class RecyclerAdapter() : RecyclerView.Adapter<ViewHolderList>() {
     val taskId = listOf("1","2","3","4","5","6")
     val taskTitleList = listOf("タスク1","タスク2","タスク3","タスク4","タスク5","タスク6")
     var taskDetailList = listOf("タスク1詳細","タスク2詳細","タスク3詳細","タスク4詳細","タスク5詳細","タスク6詳細")
-    var taskDate = listOf("タスク1詳細","タスク2詳細","タスク3詳細","タスク4詳細","タスク5詳細","タスク6詳細")
+    var taskDate = listOf("2023/01/01 01:01:01","2023/02/02 01:01:01","2023/03/03 01:01:01","2023/04/04 01:01:01","2023/05/05 01:01:01","2023/06/06 01:01:01")
+
 
     private val repository = TaskRepository()
 
@@ -65,7 +66,7 @@ class RecyclerAdapter() : RecyclerView.Adapter<ViewHolderList>() {
 
     override fun onBindViewHolder(holder: ViewHolderList, position: Int) {
         holder.task.text = taskTitleList[position]
-        holder.task.text = taskDetailList[position]
+        //holder.task.text = taskDetailList[position]
     }
 
     override fun getItemCount(): Int {

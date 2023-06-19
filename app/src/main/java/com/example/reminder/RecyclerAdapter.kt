e2fb5f1a99d625555127105f6aed00661b3940c4
tadotcom <kh.sk.ti.1820@gmail.com>
2023-06-19 21:44:30 +0900

    デイリーコミット

--- app/src/main/java/com/example/reminder/RecyclerAdapter.kt
+++ app/src/main/java/com/example/reminder/RecyclerAdapter.kt
@@ -4,6 +4,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import timber.log.Timber
 
 class RecyclerAdapter() : RecyclerView.Adapter<ViewHolderList>() {
 
@@ -14,9 +22,42 @@ class RecyclerAdapter() : RecyclerView.Adapter<ViewHolderList>() {
 
     private val repository = TaskRepository()
 
+    init {
+        val logging = HttpLoggingInterceptor {
+            Timber.tag("OkHttp").d(it)
+        }
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
+        val moshi = Moshi.Builder()
+            .build()
+
+        val userService = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(TaskService::class.java)
+
+        runBlocking {
+            launch {
+                val response = userService.getUserInfo()
+                if (response.isSuccessful) {
+                    val users = response.body()
+                    Log.d("KKKKKK1", "MOKIKIU")
+                } else {
+                    Log.d("KKKKKK2", "MOKIKIU")
+                }
+            }
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderList {
-        //データを取得する
-        //Log.d("AAAAAAAA", repository.fetchTask())
+
+
 
         val itemXml = LayoutInflater.from(parent.context).inflate(R.layout.task_list,parent,false)
         return ViewHolderList(itemXml)
--- app/src/main/java/com/example/reminder/TaskService.kt
+++ app/src/main/java/com/example/reminder/TaskService.kt
@@ -4,22 +4,25 @@ import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TaskService {
-    @GET("users/tadotcom/repos")
+    @POST("users/tadotcom/repos")
     suspend fun getUserInfo(
-        @Path("id") userName: String,
-        @Query("name") query: String
-    ): Response<UserResponse>
+        //@Path("user_name") userName: String,
+        //@Query("query") query: String
+    ): Response<String>
 }
 
 data class UserResponse(
+    // 変数名とJSONのキー名を一致させている
     val users: List<User>
 )
 
 data class User(
-    val id: Int,
-    val detail: String
+    // 変数名とJSONのキー名を一致させている
+    val id: String,
+    val node_id: String
 )
